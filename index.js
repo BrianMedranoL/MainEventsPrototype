8213f1b4a5bd494b9b170de30e7aa23d1b344336
BrianMedranoL <164426331+BrianMedranoL@users.noreply.github.com>
2024-03-28 00:06:25 -0500

    Added a bunch of customization options
    You can how change the colors of:
    Background for phone
    Top/Bottom bar
    Event Box
    Icons/Text - work in progress

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const slider = document.getElementById('slider');
 const sliderValue = document.getElementById('sliderValue');
 const events = document.querySelectorAll('.events');
+const backgroundColor = document.getElementById('colorBackground');
+const barColor = document.getElementById('colorBar');
+const eventColor = document.getElementById('colorEvent');
+const iconColor = document.getElementById('colorIcon');
+const phone = document.querySelector('.phone');
+const topBar = document.querySelector('.topbar');
+const bottomBar = document.querySelector('.bottombar');
+const date = document.querySelectorAll('.date');
+const exitIcons = document.getElementById('exit');
+const title = document.getElementById('title');
+const search = document.getElementById('search');
+
+const calander1 = document.getElementById('icon1');
+const calander2 = document.getElementById('icon2');
+const calander3 = document.getElementById('icon3');
+const calander4 = document.getElementById('icon4');
+const calander5 = document.getElementById('icon5');
 
 slider.addEventListener('input', function() {
   const value = parseInt(slider.value);
@@ -9,3 +26,33 @@ slider.addEventListener('input', function() {
     event.style.height = `${value}px`;
   });
 });
+
+eventColor.addEventListener('input', function() {
+  const color = eventColor.value;
+  events.forEach(event => {
+    event.style.backgroundColor = color;
+  });
+});
+
+backgroundColor.addEventListener('input', function() {
+  const colorz = backgroundColor.value; 
+  phone.style.backgroundColor = colorz;
+  
+});
+barColor.addEventListener('input', function() {
+  const colors = barColor.value; 
+  topBar.style.backgroundColor = colors;
+  bottomBar.style.backgroundColor = colors;
+});
+iconColor.addEventListener('input', function() {
+  const colorx = iconColor.value; 
+  exitIcons.style.color = colorx;
+  title.style.color = colorx;
+  search.style.color = colorx;
+  
+  date.forEach(event => {
+    event.style.color = colorx;
+  });
+  
+});
+
